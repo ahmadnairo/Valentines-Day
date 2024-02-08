e6f0c5cc83b4c96068dfbf235efd89ae1dc7e3bb
ahmadnairo <159453074+ahmadnairo@users.noreply.github.com>
2024-02-08 17:15:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,13 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+   "No",
+  "Damn fr fr?",
+  "wow you're not all that btw",
+  "Im jk yes you are",
+  "wallah you're all that and more",
+  "albi please say yes",
+  "My sweet cheeks say yes or you'll shart again ;(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -56,7 +47,8 @@ export default function Page() {
               style={{ width: "400x", height: "240px" }}
               src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
             />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <h1 className="text-container">Miss Hana Fouda will you be Doodie's Valentine this year, pretty
+            please?</h1>
             <div>
               <button
                 className={"yes-button"}
